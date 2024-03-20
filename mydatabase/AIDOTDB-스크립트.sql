bdfbb46de66c25630e2aa069d6eaa9fb49c4c760
AIDotLab <132873695+AIDotLab@users.noreply.github.com>
2024-03-20 20:54:36 +0900

    AIDot PoC v102 Database 스키마 개선
    AIDot PoC v102 Database 스키마 개선

--- mydatabase/AIDOTDB-스크립트.sql
+++ mydatabase/AIDOTDB-스크립트.sql
@@ -1,16 +1,23 @@
 USE AIDOTDB;
 
 /*
-DELETE FROM aidot_com_code where cod_no in ('1000000008', '1000000009');
-DELETE FROM aidot_ana_analysis where ana_analysis_no in ('1000000006', '1000000007');
+DELETE FROM aidot_ana_analysis where cod_no in ('1000000005', '1000000009');
 
+INSERT INTO aidot_ana_analysis values ('1000000001', 'c33e8032-e11f-11ee-8c68-b11980472f07', '강아지 YOLOv8 기본 모델', 'YOLOv8 기본 모델을 사용해서 강아지를 탐지합니다.', '1000000008', '1000000006', '1000000005', '2024-03-04 13:10:15', '2024-03-04 13:10:16', 'dog.jpg', 'dog.jpg', 'demo/dog.jpg', 'https://aidot2024.s3.ap-northeast-2.amazonaws.com/demo/dog.jpg', 'demo/dog-a.jpg', 'https://aidot2024.s3.ap-northeast-2.amazonaws.com/demo/dog-a.jpg', 'Y', 'Y', '1', 'Y');
+DELETE FROM aidot_ana_analysis where ana_analysis_no in ('1000000005', '1000000006');
+*/
 update aidot_ana_analysis
-   set ana_aiserver_code = '1000000006'
+   set ana_use_flag = 'Y'
  where ana_analysis_no = '1000000005'
 ;
-*/
+
+update aidot_com_aiserver 
+   set ais_status_code = 'PAUSE'
+ where ais_no in ('1000000001', '1000000002');
+;
 
 SELECT * FROM aidot_com_code;
+SELECT * FROM aidot_com_aiserver;
 SELECT * FROM aidot_ana_analysis;
 SELECT * FROM aidot_ana_analysis_v;
 
@@ -18,24 +25,6 @@ SELECT * FROM aidot_ana_analysis_v;
 -- 신규 분석No 조회
 SELECT IFNULL(MAX(ana_analysis_no), 1000000000) + 1 AS NEWNO
   FROM aidot_ana_analysis
-;
-
----------------------------------------
--- AI 서버 업데이트
--- 1000000006	AIServer	AI101
--- 1000000007	AIServer	AI102
-update aidot_ana_analysis
-   set ana_aiserver_code = '1000000006'
- where ana_analysis_no = '1000000005'
-;
-
--- 처리상태 완료로 업데이트
-update aidot_ana_analysis
-   set ana_status_code = '1000000005'
-     , ana_complete_date = SYSDATE()
-	 , ana_ana_s3_path
-	 , ana_ana_s3_url
- where ana_analysis_no = '1000000005'
 ;
 
 -- 이미지 AI 분석 재수행
@@ -51,5 +40,22 @@ update aidot_ana_analysis
 -- 이미지 AI 분석 삭제
 update aidot_ana_analysis
    set ana_use_flag = 'N'
+ where ana_analysis_no = '1000000005'
+;
+
+-- AI 서버 업데이트
+-- 1000000006	AIServer	AI101
+-- 1000000007	AIServer	AI102
+update aidot_ana_analysis
+   set ana_aiserver_code = '1000000006'
+ where ana_analysis_no = '1000000005'
+;
+
+-- 처리상태 완료로 업데이트
+update aidot_ana_analysis
+   set ana_status_code = '1000000005'
+     , ana_complete_date = SYSDATE()
+	 , ana_ana_s3_path
+	 , ana_ana_s3_url
  where ana_analysis_no = '1000000005'
 ;
--- mydatabase/AIDOTDB-스키마.sql
+++ mydatabase/AIDOTDB-스키마.sql
@@ -13,10 +13,10 @@ DROP TABLE aidot_com_code;
 
 CREATE TABLE aidot_com_code (
   cod_no INT NOT NULL,
-  cod_set VARCHAR(100) NOT NULL,
-  cod_key VARCHAR(100) NOT NULL,
-  cod_name VARCHAR(300),
-  cod_comment VARCHAR(1000),
+  cod_set VARCHAR(20) NOT NULL,
+  cod_code VARCHAR(20) NOT NULL,
+  cod_name VARCHAR(100),
+  cod_description VARCHAR(1000),
   cod_order INT,
   cod_use_flag VARCHAR(10),
   CONSTRAINT aidot_com_code_PK PRIMARY KEY(cod_no)
@@ -27,16 +27,34 @@ SELECT * FROM aidot_com_code;
 ---------------------------------------
 USE AIDOTDB;
 
+DROP TABLE aidot_com_aiserver;
+
+CREATE TABLE aidot_com_aiserver (
+  ais_no INT NOT NULL,
+  ais_code VARCHAR(20) NOT NULL,
+  ais_name VARCHAR(100),
+  ais_description VARCHAR(1000),
+  ais_order INT,
+  ais_use_flag VARCHAR(10),
+  ais_status_code VARCHAR(20),
+  CONSTRAINT aidot_com_aiserver_PK PRIMARY KEY(ais_no)
+);
+
+SELECT * FROM aidot_com_aiserver;
+
+---------------------------------------
+USE AIDOTDB;
+
 DROP TABLE aidot_ana_analysis;
 
 CREATE TABLE aidot_ana_analysis (
   ana_analysis_no INT NOT NULL,
-  ana_analysis_id VARCHAR(100) NOT NULL,
-  ana_title VARCHAR(300) NOT NULL,
+  ana_analysis_id VARCHAR(200) NOT NULL,
+  ana_title VARCHAR(100) NOT NULL,
   ana_description VARCHAR(1000),
-  ana_aimodel_code INT NOT NULL,
-  ana_aiserver_code INT NOT NULL,
-  ana_status_code INT NOT NULL,
+  ana_aimodel_code VARCHAR(20) NOT NULL,
+  ana_aiserver_code VARCHAR(20) NOT NULL,
+  ana_status_code VARCHAR(20) NOT NULL,
   ana_request_date DATETIME,
   ana_complete_date DATETIME,
   ana_org_fil_name VARCHAR(100),
@@ -66,17 +84,17 @@ SELECT A.ana_analysis_no
      , A.ana_title
      , A.ana_description
      , A.ana_aimodel_code
-     , B.cod_key AS ana_ai_key
+     , B.cod_code AS ana_ai_code
      , B.cod_name AS ana_ai_name
-     , B.cod_comment AS ana_ai_comment
+     , B.cod_description AS ana_ai_description
      , A.ana_aiserver_code
-     , C.cod_key AS ana_aiserver_key
-     , C.cod_name AS ana_aiserver_name
-     , C.cod_comment AS ana_aiserver_comment
+     , C.ais_code AS ana_aiserver_code
+     , C.ais_name AS ana_aiserver_name
+     , C.ais_description AS ana_aiserver_description
      , A.ana_status_code
-     , D.cod_key AS ana_status_key
+     , D.cod_code AS ana_status_code
      , D.cod_name AS ana_status_name
-     , D.cod_comment AS ana_status_comment
+     , D.cod_description AS ana_status_description
      , A.ana_request_date
      , A.ana_complete_date
      , A.ana_org_fil_name
@@ -92,11 +110,10 @@ SELECT A.ana_analysis_no
   FROM aidot_ana_analysis A
  INNER JOIN aidot_com_code B
     ON B.cod_set = 'AIModel'
-   AND A.ana_aimodel_code = B.cod_no
- INNER JOIN aidot_com_code C
-    ON C.cod_set = 'AIServer'
-   AND A.ana_aiserver_code = C.cod_no
+   AND A.ana_aimodel_code = B.cod_code
+ INNER JOIN aidot_com_aiserver C
+    ON A.ana_aiserver_code = C.ais_code
  INNER JOIN aidot_com_code D
     ON D.cod_set = 'AnalysisStatus'
-   AND A.ana_status_code = D.cod_no
+   AND A.ana_status_code = D.cod_code
 ;
